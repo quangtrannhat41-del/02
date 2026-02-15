d7dfea22e0b72bb221a6d7148a0f1615c212662f
Lustihgn <quangtrannhat41@gmail.com>
2026-02-15 14:52:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const papers = document.querySelectorAll(".paper");
-let current = 0;
+const balloonImages = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg"
+];
 
-document.body.addEventListener("click", () => {
-    if (current < papers.length) {
-        papers[current].classList.add("flipped");
-        current++;
-    }
-});
+function createBalloon() {
+    const balloon = document.createElement("div");
+    balloon.className = "balloon";
+
+    const img = document.createElement("img");
+    img.src = balloonImages[Math.floor(Math.random() * balloonImages.length)];
+
+    balloon.style.left = Math.random() * 90 + "vw";
+    balloon.style.animationDuration = 6 + Math.random() * 4 + "s";
+
+    balloon.appendChild(img);
+    document.body.appendChild(balloon);
+
+    setTimeout(() => {
+        balloon.remove();
+    }, 10000);
+}
+
+// tạo bóng bay liên tục
+setInterval(createBalloon, 800);
